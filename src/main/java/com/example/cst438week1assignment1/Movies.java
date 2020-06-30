eae94d4ee19d0973ae52a4feeb391ba5708bf9fa
George Blombach <george.blombach@pacificomputing.com>
2020-06-30 14:58:19 -0700

    added java.util.Date module and adjusted calling method line 37

--- src/main/java/com/example/cst438week1assignment1/Movies.java
+++ src/main/java/com/example/cst438week1assignment1/Movies.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import java.util.Date;
 
 @Controller
 public class Movies {    // localhost:8080/movies
@@ -34,7 +34,7 @@ public class Movies {    // localhost:8080/movies
     if (result.hasErrors()) {
       return "movie_form";
     }
-    movie.setDate(new java.util.Date().toString());
+    movie.setDate(new Date().toString());
     movieRepository.save(movie);
     return "movie_show";
   }
