a689c77b477ae8589e6ed2a067986bfb391e9e0a
George Blombach <george.blombach@pacificomputing.com>
2020-06-30 14:18:47 -0700

    Removed unused module RequestParam

--- src/main/java/com/example/cst438week1assignment1/Movies.java
+++ src/main/java/com/example/cst438week1assignment1/Movies.java
@@ -8,8 +8,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
-
 @Controller
 public class Movies {    // localhost:8080/movies
 
